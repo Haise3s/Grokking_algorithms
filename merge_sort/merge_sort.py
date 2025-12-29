0714bcd002f140880fdc38d3656eb7236fd3b4c5
Aleksei Oprosichev <leha.oprosichev@mail.ru>
2025-12-29 19:44:45 +0300

    end merge_sort

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,5 +1,5 @@
 a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+#b = list(map(int, input().split()))
 
 
 def merge_two_list(a: list, b: list) -> list:
@@ -18,4 +18,13 @@ def merge_two_list(a: list, b: list) -> list:
         new += b[j:]
     return new
 
+def merge_sort(a):
+    if len(a) == 1:
+        return a
+    mid = len(a)//2
+    left = merge_sort(a[0:mid])
+    right = merge_sort(a[mid:])
+    return merge_two_list(left,right)
+
+print(merge_sort(a))
 
